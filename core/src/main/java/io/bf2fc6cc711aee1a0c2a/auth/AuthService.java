9e02a35c76609aff07efb3cbc6b709ceb158b848
Carles Arnal <carlesarnal92@gmail.com>
2021-01-26 15:58:05 +0100

    Add roles property

--- core/src/main/java/io/bf2fc6cc711aee1a0c2a/auth/AuthService.java
+++ core/src/main/java/io/bf2fc6cc711aee1a0c2a/auth/AuthService.java
@@ -3,10 +3,15 @@ package io.bf2fc6cc711aee1a0c2a.auth;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class AuthService {
@@ -22,6 +27,10 @@ public class AuthService {
     @ConfigProperty(name = "auth.admin.password")
     String password;
 
+    @Inject
+    @ConfigProperty(name = "auth.realm.roles")
+    List<String> roles;
+
     Keycloak keycloak;
 
     @PostConstruct
@@ -35,20 +44,20 @@ public class AuthService {
                 .build();
     }
 
-    public RealmRepresentation createTenantAuth(String tenantId) {
-        final RealmRepresentation realmRepresentation = new RealmRepresentation();
+    public boolean createTenantAuthResources(String tenantId) {
 
-        realmRepresentation.setDisplayName(tenantId);
-        keycloak.realms()
-                .create(realmRepresentation);
 
-        return getRealm(tenantId);
+        return false;
     }
 
-    public RealmRepresentation getRealm(String name) {
 
-        return keycloak.realms()
-                .realm(name)
-                .toRepresentation();
+    /**
+     * Add realm roles
+     *
+     * @param realm name
+     */
+    public boolean addRealmRoles(String realm, List<String> roles) {
+
+        return false;
     }
 }
