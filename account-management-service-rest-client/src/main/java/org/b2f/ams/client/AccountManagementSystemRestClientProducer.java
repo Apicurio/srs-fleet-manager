f22dc191005091a4297819968d5a14d3e3b84c24
Carles Arnal <carlesarnal92@gmail.com>
2021-04-12 11:07:10 +0200

    Add config properties to ams rest client

--- account-management-service-rest-client/src/main/java/org/b2f/ams/client/AccountManagementSystemRestClientProducer.java
+++ account-management-service-rest-client/src/main/java/org/b2f/ams/client/AccountManagementSystemRestClientProducer.java
@@ -2,6 +2,8 @@ package org.b2f.ams.client;
 
 import io.quarkus.arc.profile.UnlessBuildProfile;
 import org.b2f.ams.client.auth.Auth;
+import org.b2f.ams.client.auth.KeycloakAuth;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,17 +16,29 @@ public class AccountManagementSystemRestClientProducer {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    //TODO inject auth properties and endpoint
+    @ConfigProperty(name = "account-management-system.url")
+    private String endpoint;
 
-    private String endpoint = "localhost:8900";
-    private Auth auth = null;
+    @ConfigProperty(name = "sso.url")
+    private String ssoUrl;
 
+    @ConfigProperty(name = "sso.realm")
+    private String ssoRealm;
+
+    @ConfigProperty(name = "sso.client-id")
+    private String ssoClientId;
+
+    @ConfigProperty(name = "sso.client-secret")
+    private String ssoClientSecret;
 
     @UnlessBuildProfile("test")
     @Produces
     @ApplicationScoped
     public AccountManagementSystemRestClient produce() {
         log.info("Using Account Management System REST client.");
+
+        final Auth auth = new KeycloakAuth(ssoUrl, ssoRealm, ssoClientId, ssoClientSecret);
+
         return new AccountManagementSystemRestClient(endpoint, Collections.emptyMap(), auth);
     }
 }
