b25457fa59bb4cd3f3daaadac8248fc354c8c99c
Carles Arnal <carlesarnal92@gmail.com>
2021-04-13 10:43:44 +0200

    Add account info

--- spi/src/main/java/org/bf2/srs/fleetmanager/spi/model/AccountInfo.java
+++ spi/src/main/java/org/bf2/srs/fleetmanager/spi/model/AccountInfo.java
@@ -1,4 +1,14 @@
 package org.bf2.srs.fleetmanager.spi.model;
 
 public class AccountInfo {
+
+    private final String organizationId;
+
+    public AccountInfo(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
 }
