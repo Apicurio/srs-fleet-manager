b2b926179f9134521f83dec68060b2f313d3afcf
Carles Arnal <carlesarnal92@gmail.com>
2021-05-24 13:56:06 +0200

    Fix bean producer

--- account-management-service/src/main/java/org/bf2/srs/fleetmanager/spi/impl/AccountManagementServiceProducer.java
+++ account-management-service/src/main/java/org/bf2/srs/fleetmanager/spi/impl/AccountManagementServiceProducer.java
@@ -2,7 +2,7 @@ package org.bf2.srs.fleetmanager.spi.impl;
 
 import io.quarkus.arc.profile.UnlessBuildProfile;
 import org.b2f.ams.client.AccountManagementSystemRestClient;
-import org.bf2.srs.fleetmanager.spi.EntitlementsService;
+import org.bf2.srs.fleetmanager.spi.AccountManagementService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +21,7 @@ public class AccountManagementServiceProducer {
     @UnlessBuildProfile("test")
     @Produces
     @ApplicationScoped
-    public EntitlementsService produce() {
+    public AccountManagementService produce() {
         log.info("Using Apicurio Registry REST AccountManagerClient.");
         return new AccountManagementServiceImpl(accountManagementSystemRestClient);
     }
