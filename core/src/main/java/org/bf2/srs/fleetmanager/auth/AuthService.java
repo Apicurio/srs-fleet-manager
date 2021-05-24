c364b133ee54f0db73a3cdb905624cf6fccf7ec9
Carles Arnal <carlesarnal92@gmail.com>
2021-05-24 10:04:47 +0200

    Add organization id extraction

--- core/src/main/java/org/bf2/srs/fleetmanager/auth/AuthService.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/auth/AuthService.java
@@ -1,18 +1,28 @@
 package org.bf2.srs.fleetmanager.auth;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class AuthService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    public String extractOrganizationId() {
+    @ConfigProperty(name = "srs-fleet-manager.organization-id.claim-name")
+    String organizationIdClaimName;
 
+    @Inject
+    JsonWebToken jwt;
+
+    public String extractOrganizationId() {
         log.debug("Extracting organization id from the authentication token");
-        return null;
+
+        return (String) jwt.claim(organizationIdClaimName)
+                .orElse("");
     }
 }
