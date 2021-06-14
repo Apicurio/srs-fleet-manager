d0c54a1180142a2bc5aee006583740621026f25e
Fabian Martinez <famartin@redhat.com>
2021-06-14 16:15:26 +0200

    pull sr from gitlab

--- build-project.sh
+++ build-project.sh
@@ -2,8 +2,8 @@
 
 set -eo pipefail
 
-git clone https://github.com/Apicurio/apicurio-registry.git -o upstream apicurio-registry.git
-pushd apicurio-registry.git
+git clone https://gitlab.cee.redhat.com/service-registry/srs-service-registry.git -o upstream srs-service-registry.git
+pushd srs-service-registry.git
 git checkout master
 mvn install -Pprod -Pmultitenancy -pl 'multitenancy/tenant-manager-client' -am
 popd
