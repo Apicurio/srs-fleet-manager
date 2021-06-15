bf5605cd9a1d252371df9702736ce420f362d505
Wojciech Trocki <wtrocki@redhat.com>
2021-06-15 17:14:39 +0100

    chore: delete unused class

--- core/src/main/java/org/bf2/srs/fleetmanager/util/TypeConvertUtils.java
+++
@@ -1,8 +0,0 @@
-package org.bf2.srs.fleetmanager.util;
-
-public class TypeConvertUtils {
-
-    public static <T> T getValueOrDefault(T value, T defaultValue) {
-        return value == null ? defaultValue : value;
-    }
-}
