ed004a11473cf4a5a8c1bca7292f766cfb444b8c
Wojciech Trocki <wtrocki@redhat.com>
2021-06-25 15:33:28 +0100

    fix: using oudated hibernate pattern

--- core/src/main/java/org/bf2/srs/fleetmanager/util/SearchQuery.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/util/SearchQuery.java
@@ -4,7 +4,9 @@ import javax.validation.ValidationException;
 import java.util.List;
 import java.util.Scanner;
 
-
+/**
+ * Container that transforms query string into hibernate query
+ */
 public class SearchQuery {
 
     private String query;
@@ -29,7 +31,7 @@ public class SearchQuery {
             throw new ValidationException(String.format("invalid search query key that is not matching allowed values %s ", this.allowedFields.toString()));
         }
 
-        this.query = searchExpr[0] +"=?";
+        this.query = searchExpr[0] +" = ?1";
         this.arguments = new Object[]{searchExpr[1]};
     }
 
