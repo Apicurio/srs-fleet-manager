c82ca844eaf8b38732a11e21818237966250ab9e
Wojciech Trocki <wtrocki@redhat.com>
2021-06-25 15:09:23 +0100

    feat: simple search support

--- core/src/main/java/org/bf2/srs/fleetmanager/rest/service/impl/RegistryServiceImpl.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/rest/service/impl/RegistryServiceImpl.java
@@ -15,12 +15,14 @@ import org.bf2.srs.fleetmanager.storage.ResourceStorage;
 import org.bf2.srs.fleetmanager.storage.StorageConflictException;
 import org.bf2.srs.fleetmanager.storage.sqlPanacheImpl.PanacheRegistryRepository;
 import org.bf2.srs.fleetmanager.storage.sqlPanacheImpl.model.RegistryData;
+import org.bf2.srs.fleetmanager.util.SearchQuery;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.validation.ValidationException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class RegistryServiceImpl implements RegistryService {
@@ -48,27 +50,33 @@ public class RegistryServiceImpl implements RegistryService {
     @Override
     public RegistryList getRegistries(Integer page, Integer size, String orderBy, String search) {
         PanacheQuery<RegistryData> itemsQuery;
+        // Defaults
+        var sort = Sort.by("id", Sort.Direction.Ascending);
+        page = (page != null) ? page : 0;
+        size = (size != null) ? size : 10;
+
         if (orderBy != null) {
             var order = orderBy.split(" ");
             if (order.length != 2) {
                 throw new ValidationException("invalid orderBy");
             }
             if ("asc".equals(order[1])) {
-                itemsQuery = this.registryRepository.
-                        findAll(Sort.by(order[0], Sort.Direction.Ascending));
+                sort = Sort.by(order[0], Sort.Direction.Ascending);
             } else {
-                itemsQuery = this.registryRepository.
-                        findAll(Sort.by(order[0], Sort.Direction.Descending));
+                sort = Sort.by(order[0], Sort.Direction.Descending);
             }
-        } else {
-            itemsQuery = this.registryRepository.
-                    findAll(Sort.by("id", Sort.Direction.Ascending));
         }
-        var query = itemsQuery;
-        page = (page != null) ? page : 0;
-        size = (size != null) ? size : 10;
 
-        var total = this.registryRepository.count();
+        long total;
+        if (search == null || search.isEmpty()) {
+            itemsQuery = this.registryRepository.findAll(sort);
+            total = this.registryRepository.count();
+        } else {
+            var query = new SearchQuery(search,  Arrays.asList(new String[]{"name", "status"}));
+            itemsQuery = this.registryRepository.find(query.getQuery(), sort, query.getArguments());
+            total = this.registryRepository.count();
+        }
+
         var items = itemsQuery.page(Page.of(page, size)).stream().map(convertRegistry::convert)
                 .collect(Collectors
                         .toCollection(ArrayList::new));
