fdabb93d1e5b7cb5ce8a9bf1c3b80722d5744253
Carles Arnal <carlesarnal92@gmail.com>
2021-06-28 12:03:01 +0200

    Add terms review for has entitlements call

--- account-management-service/src/main/java/org/bf2/srs/fleetmanager/spi/impl/AccountManagementServiceImpl.java
+++ account-management-service/src/main/java/org/bf2/srs/fleetmanager/spi/impl/AccountManagementServiceImpl.java
@@ -2,7 +2,9 @@ package org.bf2.srs.fleetmanager.spi.impl;
 
 import org.b2f.ams.client.AccountManagementSystemRestClient;
 import org.b2f.ams.client.model.request.ClusterAuthorization;
+import org.b2f.ams.client.model.request.TermsReview;
 import org.b2f.ams.client.model.response.ClusterAuthorizationResponse;
+import org.b2f.ams.client.model.response.ResponseTermsReview;
 import org.bf2.srs.fleetmanager.spi.AccountManagementService;
 import org.bf2.srs.fleetmanager.spi.model.AccountInfo;
 
@@ -35,18 +37,25 @@ public class AccountManagementServiceImpl implements AccountManagementService {
     @Override
     public boolean hasEntitlements(AccountInfo accountInfo, String resourceType, String clusterId) {
 
-        final ClusterAuthorization clusterAuthorization = ClusterAuthorization.builder()
-                .clusterId(clusterId)
-                .reserve(false)
-                .accountUsername(accountInfo.getAccountUsername())
-                .build();
+        boolean termsAccepted = true;
+        final TermsReview termsReview = new TermsReview();
+        termsReview.setAccountUsername(accountInfo.getAccountUsername());
+        final ResponseTermsReview responseTermsReview = restClient.termsReview(termsReview);
+        termsAccepted = !responseTermsReview.getTermsRequired();
 
-        final ClusterAuthorizationResponse clusterAuthorizationResponse = restClient.clusterAuthorization(clusterAuthorization);
+        if (termsAccepted) {
+            final ClusterAuthorization clusterAuthorization = ClusterAuthorization.builder()
+                    .clusterId(clusterId)
+                    .reserve(false)
+                    .accountUsername(accountInfo.getAccountUsername())
+                    .build();
 
-        if (clusterAuthorizationResponse.getExcessResources() == null || clusterAuthorizationResponse.getExcessResources().isEmpty()) {
-            return clusterAuthorizationResponse.getAllowed();
+            final ClusterAuthorizationResponse clusterAuthorizationResponse = restClient.clusterAuthorization(clusterAuthorization);
+            if (clusterAuthorizationResponse.getExcessResources() == null || clusterAuthorizationResponse.getExcessResources().isEmpty()) {
+                return clusterAuthorizationResponse.getAllowed();
+            }
         }
-
+        //TODO throw specific exception when terms not accepted
         return false;
     }
 }
