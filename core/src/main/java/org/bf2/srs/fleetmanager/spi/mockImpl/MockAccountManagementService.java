1ee48b6c4f10a071f4ab2a79f7a7cfe74ce4e55b
Carles Arnal <carlesarnal92@gmail.com>
2021-06-28 10:10:29 +0200

    Fix mock implementation

--- core/src/main/java/org/bf2/srs/fleetmanager/spi/mockImpl/MockAccountManagementService.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/spi/mockImpl/MockAccountManagementService.java
@@ -5,9 +5,14 @@ import org.bf2.srs.fleetmanager.spi.model.AccountInfo;
 
 public class MockAccountManagementService implements AccountManagementService {
 
+    @Override
+    public String createResource(AccountInfo accountInfo, String resourceType, String clusterId, String productId) {
+        return "EMPTY_RESOURCE";
+    }
+
     //Just return true for the entitlements check call
     @Override
-    public boolean hasEntitlements(AccountInfo accountInfo, String action, String resourceType, String subscriptionId) {
+    public boolean hasEntitlements(AccountInfo accountInfo, String resourceType, String clusterId) {
         return true;
     }
 }
