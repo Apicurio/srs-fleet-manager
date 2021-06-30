5a0cbe46c87e4c6eee5852e1d49997468a8d1999
Jakub Senko <jsenko@redhat.com>
2021-06-30 14:11:04 +0200

    Remove @SneakyThrows (causes ArcUndeclaredThrowableException)

--- core/src/main/java/org/bf2/srs/fleetmanager/rest/publicapi/ApiResource.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/rest/publicapi/ApiResource.java
@@ -1,5 +1,11 @@
 package org.bf2.srs.fleetmanager.rest.publicapi;
 
+import org.bf2.srs.fleetmanager.rest.publicapi.beans.RegistryCreateRest;
+import org.bf2.srs.fleetmanager.rest.publicapi.beans.RegistryListRest;
+import org.bf2.srs.fleetmanager.rest.publicapi.beans.RegistryRest;
+import org.bf2.srs.fleetmanager.storage.RegistryNotFoundException;
+import org.bf2.srs.fleetmanager.storage.StorageConflictException;
+
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.ws.rs.Consumes;
@@ -10,50 +16,47 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import org.bf2.srs.fleetmanager.rest.publicapi.beans.RegistryCreateRest;
-import org.bf2.srs.fleetmanager.rest.publicapi.beans.RegistryListRest;
-import org.bf2.srs.fleetmanager.rest.publicapi.beans.RegistryRest;
 
 /**
  * A JAX-RS interface.  An implementation of this interface must be provided.
  */
 @Path("/api")
 public interface ApiResource {
-  /**
-   *
-   */
-  @Path("/serviceregistry_mgmt/v1/registries")
-  @GET
-  @Produces("application/json")
-  RegistryListRest getRegistries(@QueryParam("page") @Min(0) Integer page, @Min(1) @Max(1000) @QueryParam("size") Integer size,
-      @QueryParam("orderBy") String orderBy, @QueryParam("search") String search);
+    /**
+     *
+     */
+    @Path("/serviceregistry_mgmt/v1/registries")
+    @GET
+    @Produces("application/json")
+    RegistryListRest getRegistries(@QueryParam("page") @Min(0) Integer page, @Min(1) @Max(1000) @QueryParam("size") Integer size,
+                                   @QueryParam("orderBy") String orderBy, @QueryParam("search") String search);
 
-  /**
-   *
-   */
-  @Path("/serviceregistry_mgmt/v1/registries")
-  @POST
-  @Produces("application/json")
-  @Consumes("application/json")
-  RegistryRest createRegistry(RegistryCreateRest data);
+    /**
+     *
+     */
+    @Path("/serviceregistry_mgmt/v1/registries")
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    RegistryRest createRegistry(RegistryCreateRest data) throws StorageConflictException;
 
-  /**
-   * Gets the details of a single instance of a `Registry`.
-   */
-  @Path("/serviceregistry_mgmt/v1/registries/{id}")
-  @GET
-  @Produces("application/json")
-  RegistryRest getRegistry(@PathParam("id") String id);
+    /**
+     * Gets the details of a single instance of a `Registry`.
+     */
+    @Path("/serviceregistry_mgmt/v1/registries/{id}")
+    @GET
+    @Produces("application/json")
+    RegistryRest getRegistry(@PathParam("id") String id) throws RegistryNotFoundException;
 
-  /**
-   * Deletes an existing `Registry`.
-   */
-  @Path("/serviceregistry_mgmt/v1/registries/{id}")
-  @DELETE
-  void deleteRegistry(@PathParam("id") String id);
+    /**
+     * Deletes an existing `Registry`.
+     */
+    @Path("/serviceregistry_mgmt/v1/registries/{id}")
+    @DELETE
+    void deleteRegistry(@PathParam("id") String id) throws StorageConflictException, RegistryNotFoundException;
 
-  @Path("/serviceregistry_mgmt/v1/openapi")
-  @GET
-  @Produces("application/json")
-  String getSchema();
+    @Path("/serviceregistry_mgmt/v1/openapi")
+    @GET
+    @Produces("application/json")
+    String getSchema();
 }
