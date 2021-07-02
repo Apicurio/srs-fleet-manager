7d465c00b1a6fa30a4df3ebadfd64758dfd70f87
Jakub Senko <jsenko@redhat.com>
2021-07-02 12:06:09 +0200

    Revert resource loading path

--- core/src/main/java/org/bf2/srs/fleetmanager/rest/privateapi/impl/ApiResourceImpl.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/rest/privateapi/impl/ApiResourceImpl.java
@@ -22,7 +22,7 @@ public class ApiResourceImpl implements ApiResource {
 
     static {
         try {
-            SCHEMA = new String(ApiResourceImpl.class.getResourceAsStream("./srs-fleet-manager-private.json").readAllBytes());
+            SCHEMA = new String(ApiResourceImpl.class.getResourceAsStream("/srs-fleet-manager-private.json").readAllBytes());
         } catch (Exception e) {
             throw new IllegalStateException("Could not load Open API schema for the v1 REST interface.", e);
         }
