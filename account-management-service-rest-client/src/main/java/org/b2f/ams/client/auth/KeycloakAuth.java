ce9a3b698fa2037207276565367e46ca38aaefcd
Carles Arnal <carlesarnal92@gmail.com>
2021-07-20 11:51:15 +0200

    Cache access token

--- account-management-service-rest-client/src/main/java/org/b2f/ams/client/auth/KeycloakAuth.java
+++ account-management-service-rest-client/src/main/java/org/b2f/ams/client/auth/KeycloakAuth.java
@@ -1,7 +1,11 @@
 package org.b2f.ams.client.auth;
 
+import org.keycloak.TokenVerifier;
 import org.keycloak.authorization.client.AuthzClient;
 import org.keycloak.authorization.client.Configuration;
+import org.keycloak.common.VerificationException;
+import org.keycloak.common.util.Time;
+import org.keycloak.representations.AccessToken;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +15,8 @@ public class KeycloakAuth implements Auth {
     private static final String BEARER = "Bearer ";
 
     private final AuthzClient keycloak;
+    private AccessToken accessTokenParsed;
+    private String accessToken;
 
     public KeycloakAuth(String serverUrl, String realm, String clientId, String clientSecret) {
 
@@ -22,7 +28,23 @@ public class KeycloakAuth implements Auth {
 
     @Override
     public void apply(Map<String, String> requestHeaders) {
-        requestHeaders.put("Authorization", BEARER + this.keycloak.obtainAccessToken().getToken());
+        try {
+            if (isAccessTokenRequired()) {
+                this.accessToken = this.keycloak.obtainAccessToken().getToken();
+                this.accessTokenParsed = TokenVerifier.create(this.accessToken, AccessToken.class).getToken();
+            }
+            requestHeaders.put("Authorization", BEARER + accessToken);
+        } catch (VerificationException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private boolean isAccessTokenRequired() {
+        return null == accessTokenParsed || isTokenExpired();
+    }
+
+    private boolean isTokenExpired() {
+        return (accessTokenParsed.getExp() != null && accessTokenParsed.getExp() != 0L) && (long) Time.currentTime() > accessTokenParsed.getExp();
     }
 
     public static class Builder {
