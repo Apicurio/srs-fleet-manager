d68fda8ab63ecde6c7ca5433373a842805303542
Carles Arnal <carlesarnal92@gmail.com>
2021-08-03 13:03:52 +0200

    Remove keycloak dependency

--- core/src/main/java/org/bf2/srs/fleetmanager/execution/impl/workers/ProvisionRegistryTenantWorker.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/execution/impl/workers/ProvisionRegistryTenantWorker.java
@@ -96,6 +96,7 @@ public class ProvisionRegistryTenantWorker extends AbstractWorker {
                     .tenantId(registry.getTenantId())
                     .createdBy(registry.getOwner())
                     .organizationId(registry.getOrgId())
+                    .resources(plansService.getDefaultQuotaPlan().getResources())
                     .build();
 
             TenantManager tenantManager = createTenantManager(registryDeployment);
