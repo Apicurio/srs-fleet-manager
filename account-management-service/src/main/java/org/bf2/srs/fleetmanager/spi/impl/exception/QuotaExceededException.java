5f12729936f72155c95f0b0feab0c6eb921bab9f
Carles Arnal <carlesarnal92@gmail.com>
2021-08-26 16:16:31 +0200

    fix: remove unused exception

--- account-management-service/src/main/java/org/bf2/srs/fleetmanager/spi/impl/exception/QuotaExceededException.java
+++
@@ -1,8 +0,0 @@
-package org.bf2.srs.fleetmanager.spi.impl.exception;
-
-public class QuotaExceededException extends AccountManagementSystemClientException {
-
-    public QuotaExceededException(String accountId) {
-        super(String.format("Terms not accepted by account with id %s", accountId));
-    }
-}
