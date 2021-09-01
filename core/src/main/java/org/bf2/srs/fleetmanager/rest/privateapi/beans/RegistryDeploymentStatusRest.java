82e4b70b08e38e63120e4e0a60935d01a40a0607
Eric Wittmann <eric.wittmann@gmail.com>
2021-09-01 11:16:09 -0400

    Fixed iso 8601 date format again

--- core/src/main/java/org/bf2/srs/fleetmanager/rest/privateapi/beans/RegistryDeploymentStatusRest.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/rest/privateapi/beans/RegistryDeploymentStatusRest.java
@@ -26,7 +26,7 @@ public class RegistryDeploymentStatusRest {
      * (Required)
      * 
      */
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ", timezone = "UTC")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @JsonProperty("lastUpdated")
     @JsonPropertyDescription("ISO 8601 UTC timestamp.")
     private Date lastUpdated;
