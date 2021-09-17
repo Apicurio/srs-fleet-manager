f1de034ed02bc14e601a0b8ebb509f6ab6195b58
Jakub Senko <jsenko@redhat.com>
2021-09-17 14:45:14 +0200

    fix: update REST API to reflect global count change from eval to all instances

--- core/src/main/java/org/bf2/srs/fleetmanager/rest/publicapi/beans/ServiceStatus.java
+++ core/src/main/java/org/bf2/srs/fleetmanager/rest/publicapi/beans/ServiceStatus.java
@@ -16,35 +16,35 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "max_eval_instances_reached"
+    "max_instances_reached"
 })
 @Generated("jsonschema2pojo")
 public class ServiceStatus {
 
     /**
-     * Boolean property indicating if the maximum number of Trial instances have been reached, therefore creation of more eval instances should not be allowed.
+     * Boolean property indicating if the maximum number of total instances have been reached, therefore creation of more instances should not be allowed.
      * 
      */
-    @JsonProperty("max_eval_instances_reached")
-    @JsonPropertyDescription("Boolean property indicating if the maximum number of Trial instances have been reached, therefore creation of more eval instances should not be allowed.")
-    private Boolean maxEvalInstancesReached;
+    @JsonProperty("max_instances_reached")
+    @JsonPropertyDescription("Boolean property indicating if the maximum number of total instances have been reached, therefore creation of more instances should not be allowed.")
+    private Boolean maxInstancesReached;
 
     /**
-     * Boolean property indicating if the maximum number of Trial instances have been reached, therefore creation of more eval instances should not be allowed.
+     * Boolean property indicating if the maximum number of total instances have been reached, therefore creation of more instances should not be allowed.
      * 
      */
-    @JsonProperty("max_eval_instances_reached")
-    public Boolean getMaxEvalInstancesReached() {
-        return maxEvalInstancesReached;
+    @JsonProperty("max_instances_reached")
+    public Boolean getMaxInstancesReached() {
+        return maxInstancesReached;
     }
 
     /**
-     * Boolean property indicating if the maximum number of Trial instances have been reached, therefore creation of more eval instances should not be allowed.
+     * Boolean property indicating if the maximum number of total instances have been reached, therefore creation of more instances should not be allowed.
      * 
      */
-    @JsonProperty("max_eval_instances_reached")
-    public void setMaxEvalInstancesReached(Boolean maxEvalInstancesReached) {
-        this.maxEvalInstancesReached = maxEvalInstancesReached;
+    @JsonProperty("max_instances_reached")
+    public void setMaxInstancesReached(Boolean maxInstancesReached) {
+        this.maxInstancesReached = maxInstancesReached;
     }
 
 }
