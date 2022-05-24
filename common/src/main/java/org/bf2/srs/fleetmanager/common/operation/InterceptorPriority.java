c3a1ca7051a1763679e979a27a715d18f8b30efe
Jakub Senko <jsenko@redhat.com>
2022-05-24 12:36:51 +0200

    refactor: do not use an interface for constants

--- common/src/main/java/org/bf2/srs/fleetmanager/common/operation/InterceptorPriority.java
+++ common/src/main/java/org/bf2/srs/fleetmanager/common/operation/InterceptorPriority.java
@@ -5,27 +5,30 @@ import javax.interceptor.Interceptor;
 /**
  * @author Jakub Senko <m@jsenko.net>
  */
-public interface InterceptorPriority {
+public class InterceptorPriority {
+
+    private InterceptorPriority() {
+    }
 
     // ^^^ Caller
 
-    int AUDITING = Interceptor.Priority.APPLICATION - 100; // = 1900
+    public static final int AUDITING = Interceptor.Priority.APPLICATION - 100; // = 1900
 
-    int CHECK_READ_PERMISSIONS = Interceptor.Priority.APPLICATION - 99;
+    public static final int CHECK_READ_PERMISSIONS = Interceptor.Priority.APPLICATION - 99;
 
-    int CHECK_DELETE_PERMISSIONS = Interceptor.Priority.APPLICATION - 98;
+    public static final int CHECK_DELETE_PERMISSIONS = Interceptor.Priority.APPLICATION - 98;
 
-    int READ_ONLY_MODE = Interceptor.Priority.APPLICATION - 97;
+    public static final int READ_ONLY_MODE = Interceptor.Priority.APPLICATION - 97;
 
-    int LOGGING = Interceptor.Priority.APPLICATION - 96;
+    public static final int LOGGING = Interceptor.Priority.APPLICATION - 96;
 
     // ~~~ Application Priority
 
-    int RETRY_UNWRAP = 4000;
+    public static final int RETRY_UNWRAP = 4000;
 
     // io.smallrye.faulttolerance.FaultToleranceInterceptor = 4010
 
-    int RETRY_WRAP = 4020;
+    public static final int RETRY_WRAP = 4020;
 
     // vvv Target
 }
