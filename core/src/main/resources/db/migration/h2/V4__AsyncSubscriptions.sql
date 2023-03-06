31c3e86596b96a25f8da303de3a722462d34bc3a
Carles Arnal <carlesarnal92@gmail.com>
2023-03-06 11:34:22 +0100

    fix: sql upgrade scripts

--- core/src/main/resources/db/migration/h2/V4__AsyncSubscriptions.sql
+++ core/src/main/resources/db/migration/h2/V4__AsyncSubscriptions.sql
@@ -1,2 +1,2 @@
-alter table registry modify column instance_type varchar(255) null;
+alter table registry alter column INSTANCE_TYPE DROP NOT NULL;
 -- Index? create index idx_registry_5 on registry (instance_type);
